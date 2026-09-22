bc57cad3f83e09b5bac7b27f4b4a2243d7c0889c
Francisco Gonzalez Caravia <fagonzalezcaravia@gmail.com>
2026-09-21 21:41:07 -0300

    Add Plenario photo assets

--- scripts/fetch-assets.mjs
+++ scripts/fetch-assets.mjs
@@ -6,6 +6,14 @@ const assets = [
   { name: 'santa-fe-sin-hambre.jpg', id: '1ZHbq2rAz9S3PY9Rvhkm6ygMrBMNzkJwX', min: 200000 },
   { name: 'actividad.jpg', id: '1Yjil6-BYX4P3OvO8SkYRZmFyIt2Bp685', min: 500000 },
   { name: 'organizacion.jpg', id: '1_BQBUajl1WP_mQhq3cBy5ZDRRTgFBlKZ', min: 500000 },
+  { name: 'plenario-11.jpg', id: '1af2Z_k0fyXk5oQ11kKjN17jJu6JpiDbl', min: 500000 },
+  { name: 'plenario-15.jpg', id: '1kHoW0PZ3jvDlVHKDpQqqB1oYKhnhkcrb', min: 500000 },
+  { name: 'plenario-17.jpg', id: '1cS61G1Z_IiABL5majT58n7jwCgWo3idl', min: 500000 },
+  { name: 'plenario-1.jpg', id: '1JNb1hSh8cD_plHK8rOs3fI-cMkbjQCU5', min: 500000 },
+  { name: 'plenario-3.jpg', id: '1T8Xnom5eNIJwxlAqhDqmJTttC-jnt4gU', min: 500000 },
+  { name: 'plenario-7.jpg', id: '1wSdRx4vEaeofvEoUTd4VwDsf-2_EDblt', min: 500000 },
+  { name: 'plenario-20.jpg', id: '1cQHnOwKIA4NhpaN1wTQWiOpedjmrpBSn', min: 500000 },
+  { name: 'plenario-23.jpg', id: '1KYVssqFNGzSc-dhN_pV2QjABW1bydmgi', min: 500000 },
   { name: 'logo.png', id: '1Vyaw-61UKtqBYUKmPu9KwnOBaFehErXc', min: 5000 },
   { name: 'logo-white.png', id: '10CZgCff-5dgUg-327k5reAXJjDy102a4', min: 5000 }
 ];
